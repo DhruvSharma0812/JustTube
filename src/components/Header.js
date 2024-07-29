5dcdab38cf6bc39e11f28b90c383e59dd8027439
Dhruv Sharma <sharmdhruv1234@gmail.com>
2024-07-29 19:21:57 +0530

    Navbar and Sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,53 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { toggleMenu } from '../utils/appSlice';
 
 const Header = () => {
+
+  const dispatch = useDispatch();
+  const toggleMenuHandler = () => {
+    dispatch (toggleMenu())
+  }
+
   return (
-    <div>
-        Header
+    <div className='grid grid-flow-col p-3 shadow-lg bg-white items-center'>
+
+      {/* Left Section with Hamburger Logo and Youtube Logo */}
+      <div className='flex items-center col-span-1 gap-4'>
+        <img
+          onClick={() => toggleMenuHandler()}
+          className='h-6 cursor-pointer'
+          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAARVBMVEX///8jHyAgHB0OBQgMAAWlpKQpJSaenZ309PUAAAAIAAD8/Pz5+fna2tqop6dvbW1oZmevrq4tKivFxMQYExRiYGC+vr7Dc4WrAAABB0lEQVR4nO3cS3LCMBAFQGIIIBPbhN/9jxqSyiIsTUnlydB9g1eSNV5MvdUKAAAAAAAAAAAAAAAAXtEwvscwDk3yHabSb2Loy/TRIOHUv8XRH+sHHMrSqR6U+hd1jHSE90P8lHC2/Lc0/0vzMy3WMdynxaFBwu+Jv4uh0cQHAAAAAAAAAIB59jG0ijdcT9sYTtcmK0PncumiuJRz/YD7bbf0ut4f3br+GvQt2PblrXrC3WbpUA/6sXrC/GeY/zvM/5aGmofHZiu0S//M/GoVDwAAAAAAAAAAZsjeuRerN1HL7hPy95fm76DNnzD/Lc3/0rxAJ3v+Xn0AAAAAAAAAAAAAAAD4T74AYhs1O+vt3ioAAAAASUVORK5CYII="
+          alt="Youtube Hamburger img"
+        />
+
+        <img
+          className='h-6 cursor-pointer'
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
+          alt="Youtube Logo"
+        />
+      </div>
+
+      {/* Middle Section with Search Bar and Search Icon  */}
+      <div className="col-span-10 flex justify-center items-center">
+        <input
+          className="w-1/2 p-2 px-5 border border-gray-700 rounded-l-3xl focus:outline-none focus:ring-2 focus:ring-blue-500"
+          type="text"
+          placeholder="Search"
+        />
+        <button className=" p-3 bg-gray-100 hover:bg-gray-300 border border-gray-700 rounded-r-3xl focus:outline-none focus:ring-2 focus:ring-blue-500">
+          <img className='h-4' src="https://static-00.iconduck.com/assets.00/search-icon-2048x2048-cmujl7en.png" alt="" />
+        </button>
+      </div>
+
+
+      {/* Last Sectino with User icon  */}
+      <div className='col-span-1'>
+      <lord-icon
+    src="https://cdn.lordicon.com/hrjifpbq.json"
+    trigger="hover">
+</lord-icon>
+      </div>
     </div>
   )
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,134 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import store from "../utils/store";
 
 const Sidebar = () => {
-  return (
-    <div>
-        Sidebar
-    </div>
-  )
-}
 
-export default Sidebar
+  const isMenuOpen = useSelector ((store) => store.app.isMenuOpen)
+
+  if (!isMenuOpen) {
+    return null;
+  }
+
+  return (
+    <div className="p-5 shadow-lg w-48 bg-white overflow-y-auto">
+      <ul className="">
+        <li className="flex items-center gap-2 text-gray-700 bg-gray-100 hover:bg-gray-100 p-2 rounded-md cursor-pointer">
+          <lord-icon
+            style={{ width: "24px", height: "24px" }}
+            src="https://cdn.lordicon.com/wmwqvixz.json"
+            trigger="hover"
+          ></lord-icon>
+          Home
+        </li>
+
+        <li className="flex items-center gap-2 text-gray-700 hover:bg-gray-100 p-2 rounded-md cursor-pointer">
+          <lord-icon
+            src="https://cdn.lordicon.com/qisgortv.json"
+            trigger="hover"
+            style={{ width: "24px", height: "24px" }}
+          ></lord-icon>
+          Shorts
+        </li>
+
+        <li className="flex items-center gap-2 text-gray-700 hover:bg-gray-100 p-2 rounded-md cursor-pointer">
+          <lord-icon
+            src="https://cdn.lordicon.com/gewhxiwb.json"
+            trigger="hover"
+            style={{ width: "24px", height: "24px" }}>
+          </lord-icon>
+          Videos
+        </li>
+
+        <li className="flex items-center gap-2 text-gray-700 hover:bg-gray-100 p-2 rounded-md cursor-pointer">
+          <lord-icon
+            src="https://cdn.lordicon.com/uwtqzoif.json"
+            trigger="hover"
+            style={{ width: "24px", height: "24px" }}
+          ></lord-icon>
+          Live
+        </li>
+      </ul>
+      <hr className="my-1 border-t-1 border-black" />
+
+      <h1 className="font-bold pt-5 pb-2 text-gray-900">Explore</h1>
+      <ul className="">
+        <li className="flex items-center gap-2 text-gray-700 hover:bg-gray-100 p-2 rounded-md cursor-pointer">
+          <span className="font-bold h-6 px-2">#</span> Trending
+        </li>
+        <li className="flex items-center gap-2 text-gray-700 hover:bg-gray-100 p-2 rounded-md cursor-pointer">
+          <lord-icon
+            src="https://cdn.lordicon.com/cosvjkbu.json"
+            trigger="hover"
+            style={{ width: "24px", height: "24px" }}>
+          </lord-icon>
+          Shopping
+        </li>
+        <li className="flex items-center gap-2 text-gray-700 hover:bg-gray-100 p-2 rounded-md cursor-pointer">
+          <lord-icon
+            src="https://cdn.lordicon.com/ihajlcqd.json"
+            trigger="hover"
+            style={{ width: "24px", height: "24px" }}>
+          </lord-icon>
+          Fashion & beauty
+        </li>
+        <li className="flex items-center gap-2 text-gray-700 hover:bg-gray-100 p-2 rounded-md cursor-pointer">
+          <lord-icon
+            src="https://cdn.lordicon.com/ceogkocw.json"
+            trigger="hover"
+            style={{ width: "24px", height: "24px" }}>
+          </lord-icon>
+          Podcasts
+        </li>
+        <li className="flex items-center gap-2 text-gray-700 hover:bg-gray-100 p-2 rounded-md cursor-pointer">
+          <lord-icon
+            src="https://cdn.lordicon.com/gemrrbjz.json"
+            trigger="hover"
+            style={{ width: "24px", height: "24px" }}>
+          </lord-icon>
+          News
+        </li>
+      </ul>
+
+      <hr className="my-1 border-t-1 border-black" />
+      <h1 className="font-bold pt-5 pb-2 text-gray-900">More from Youtube</h1>
+      <ul className="">
+        <li className="flex items-center gap-2 text-gray-700 hover:bg-gray-100 p-2 rounded-md cursor-pointer">
+          <lord-icon
+            src="https://cdn.lordicon.com/yvsfcbjq.json"
+            trigger="hover"
+            style={{ width: "24px", height: "24px" }}>
+          </lord-icon>
+          Music
+        </li>
+        <li className="flex items-center gap-2 text-gray-700 hover:bg-gray-100 p-2 rounded-md cursor-pointer">
+          <lord-icon
+            src="https://cdn.lordicon.com/jdngjjzg.json"
+            trigger="hover"
+            style={{ width: "24px", height: "24px" }}>
+          </lord-icon>
+          Sports
+        </li>
+        <li className="flex items-center gap-2 text-gray-700 hover:bg-gray-100 p-2 rounded-md cursor-pointer">
+          <lord-icon
+            src="https://cdn.lordicon.com/nkmrdfzh.json"
+            trigger="hover"
+            style={{ width: "24px", height: "24px" }}>
+          </lord-icon>
+          Gaming
+        </li>
+        <li className="flex items-center gap-2 text-gray-700 hover:bg-gray-100 p-2 rounded-md cursor-pointer">
+          <lord-icon
+            src="https://cdn.lordicon.com/wwdlquyd.json"
+            trigger="hover"
+            style={{ width: "24px", height: "24px" }}>
+          </lord-icon>
+          Movies
+        </li>
+      </ul>
+
+    </div>
+  );
+};
+
+export default Sidebar;
